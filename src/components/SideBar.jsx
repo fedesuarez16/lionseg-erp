a0be538133b914083e3746d5be4d2c5c2638b779
fedesuarez16 <fedesuarez16@gmail.com>
2024-06-19 01:07:21 -0300

    :idk

--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -2,6 +2,8 @@
 import React, { useState } from 'react';
 import logo from './logo.png'; // Importa tu logo aquí
 import { Link } from 'react-router-dom'; // Import Link
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBars, faChevronLeft, faChevronRight, faSignOutAlt } from '@fortawesome/free-solid-svg-icons'; // Importa los iconos
 
 const Sidebar = () => {
   const [isSidebarOpen, setIsSidebarOpen] = useState(true);
@@ -10,45 +12,69 @@ const Sidebar = () => {
     setIsSidebarOpen(!isSidebarOpen);
   };
 
+  const handleLogout = () => {
+    // Implementa tu lógica de cerrar sesión aquí
+    console.log("Cerrando sesión");
+  };
+
   return (
-    <div className={`bg-gray-900 text-white w-${isSidebarOpen ? '64' : '16'} p-4 transition-all`}>
-      {/* Logo */}
-      <div className="flex justify-start items-center p-4 mb-4">
-        <img src={logo} alt="Logo" className="w-40 h-16" /> {/* Ajusta el tamaño del logo según tus necesidades */}
+    <div className={`bg-gray-900 text-white ${isSidebarOpen ? 'w-64' : 'w-20'} min-h-screen flex flex-col justify-between transition-all duration-300`}>
+      <div>
+        {/* Logo and Toggle Button */}
+        <div className="flex justify-between items-center p-4 mb-4">
+          <img src={logo} alt="Logo" className={`${isSidebarOpen ? 'w-18 h-10 mr-2' : 'hidden'}`}/> {/* Ajusta el tamaño del logo según tus necesidades */}
+          <button
+            className="bg-gray-800 text-white hover:bg-gray-700 p-2 rounded focus:outline-none"
+            onClick={handleSidebarToggle}
+          >
+            <FontAwesomeIcon icon={isSidebarOpen ? faBars : faBars} />
+          </button>
+        </div>
+
+        {/* Sidebar Content */}
+        {isSidebarOpen && (
+          <ul className="space-y-2">
+            <li>
+              <Link to="/clients" className="block text-white hover:bg-gray-700 p-2 rounded transition-colors duration-300">
+                Lista de Clientes
+              </Link>
+            </li>
+            <li>
+              <Link to="/facturas" className="block text-white hover:bg-gray-700 p-2 rounded transition-colors duration-300">
+                Lista de Facturas
+              </Link>
+            </li>
+            <li>
+              <Link to="/transactions" className="block text-white hover:bg-gray-700 p-2 rounded transition-colors duration-300">
+                Lista de Transacciones
+              </Link>
+            </li>
+            <li>
+              <Link to="/reportes" className="block text-white hover:bg-gray-700 p-2 rounded transition-colors duration-300">
+                Reportes
+              </Link>
+            </li>
+          </ul>
+        )}
+        {!isSidebarOpen && (
+          <button
+            className="fixed bottom-20 bg-gray-800 text-white hover:bg-gray-700 p-2 rounded focus:outline-none"
+            onClick={handleSidebarToggle}
+          >
+            <FontAwesomeIcon icon={faChevronRight} />
+          </button>
+        )}
       </div>
-
-      {/* Sidebar Content */}
-      {isSidebarOpen && (
-        <ul className='mx-auto text-center'>
-          <li className="mb-2">
-            <Link to="/clients" className="block text-white hover:bg-blue-600 p-2 rounded">
-              Lista de Clientes
-            </Link>
-          </li>
-          <li className="mb-2">
-            <Link to="/facturas" className="block text-white hover:bg-blue-600 p-2 rounded">
-              Lista de Facturas
-            </Link>
-          </li>
-          <li className="mb-2">
-            <Link to="/clients" className="block text-white hover:bg-blue-600 p-2 rounded">
-              Lista de Transacciones
-            </Link>
-          </li>
-          <li className="mb-2">
-            <Link to="/reportes" className="block text-white hover:bg-blue-600 p-2 rounded">
-              Reportes
-            </Link>
-          </li>
-        </ul>
-      )}
-
-      {/* Sidebar Toggle Button */}
-      <button
-        className="bg-white text-black hover:bg-gray-700 p-2 rounded"
-        onClick={handleSidebarToggle}
-      >
-      </button>
+      {/* Logout Button */}
+      <div className="p-4">
+        <button
+          className="w-full flex items-center bg-gray-700 text-white hover:bg-red-700 p-2 rounded focus:outline-none"
+          onClick={handleLogout}
+        >
+          <FontAwesomeIcon icon={faSignOutAlt} className="mr-2" />
+          {isSidebarOpen && <span>Cerrar Sesión</span>}
+        </button>
+      </div>
     </div>
   );
 };
