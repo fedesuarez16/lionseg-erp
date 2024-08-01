e72844b99408f9c0e5620f7f41cca6165ec49e6d
fedesuarez16 <fedesuarez16@gmail.com>
2024-08-01 14:57:34 -0300

    idk

--- src/components/InvoiceModal.jsx
+++ src/components/InvoiceModal.jsx
@@ -17,7 +17,6 @@ const InvoiceModal = ({ clientId, onClose, onInvoiceCreated }) => {
     try {
       const response = await axios.post(`https://lionseg-df2520243ed6.herokuapp.com/api/clientes/${clientId}/invoices`, {
         monto,
-        destinatario: clientId,
         fechaFactura,
         fechaVencimiento,
         descripcion,
