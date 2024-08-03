0b122087e61d8935930b830e1768eb084539bf52
fedesuarez16 <fedesuarez16@gmail.com>
2024-08-03 01:30:22 -0300

    idk

--- src/components/ClientList.jsx
+++ src/components/ClientList.jsx
@@ -57,7 +57,7 @@ const ClientList = () => {
   }
 
   return (
-    <div className="p-2 border rounded-xl min-h-screen h-auto relative">
+    <div className="p-2 bg-white border rounded-xl min-h-screen h-auto relative">
       <Navbar onSearch={handleSearch} />
       <button
         className="fixed bottom-4 right-6 pb-[14px] bg-gray-800 text-white py-2 px-[18px] rounded-full text-3xl"
@@ -66,26 +66,26 @@ const ClientList = () => {
       </button>
 
       {isAddClientVisible && <AddClient onClientAdded={handleClientAdded} />}
-      <table className="min-w-full bg-white  border rounded border-gray-200">
+      <table className="min-w-full bg-white rounded-lg border border-collapse border-gray-200">
         <thead>
           <tr className="text-gray-500 m-2">
-            <th className="border-t border-b border-gray-400 p-4 rounded mx-2">Name</th>
-            <th className="border-t border-b border-gray-400 p-4 rounded mx-2">Email</th>
-            <th className="border-t border-b border-gray-400 p-4 rounded mx-2">Phone Number</th>
-            <th className="border-t border-b border-gray-400 p-4 rounded mx-2">Creation Date</th>
-            <th className="border-t border-b border-gray-400 p-4 rounded mx-2">State</th>
+            <th className="border-t border-b border-gray-400 p-4 rounded-tl-lg mx-2">Name</th>
+            <th className="border-t border-b border-gray-400 p-4 mx-2">Email</th>
+            <th className="border-t border-b border-gray-400 p-4 mx-2">Phone Number</th>
+            <th className="border-t border-b border-gray-400 p-4 mx-2">Creation Date</th>
+            <th className="border-t border-b border-gray-400 p-4 rounded-tr-lg mx-2">State</th>
           </tr>
         </thead>
         <tbody>
-          {clients.map((client) => (
-            <tr key={client._id}>
-              <td className="border-t border-b border-gray-400 p-4 rounded-bl mx-2">
+          {clients.map((client, index) => (
+            <tr className='text-gray-600' key={client._id}>
+              <td className={`border-t border-b border-gray-400 p-4 mx-2 ${index === clients.length - 1 ? 'rounded-bl-lg' : ''}`}>
                 <Link to={`/clients/${client._id}`}>{client.name}</Link>
               </td>
               <td className="border-t border-b border-gray-400 p-4 mx-2">{client.email}</td>
               <td className="border-t border-b border-gray-400 p-4 mx-2">{client.phoneNumber}</td>
               <td className="border-t border-b border-gray-400 p-4 mx-2">{new Date(client.creationDate).toLocaleDateString()}</td>
-              <td className="border-t border-b border-gray-400 p-4 mx-2">
+              <td className={`border-t border-b border-gray-400 p-4 mx-2 ${index === clients.length - 1 ? 'rounded-br-lg' : ''}`}>
                 <select
                   value={client.state}
                   onChange={(e) => handleStateChange(client._id, e.target.value)}
